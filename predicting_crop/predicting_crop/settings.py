d1f4df76729b47589415ca7b04f8016de8140819
Sumit <108853577+Sumitwarrior7@users.noreply.github.com>
2023-03-23 10:27:42 +0530

    Update settings.py

--- predicting_crop/predicting_crop/settings.py
+++ predicting_crop/predicting_crop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!&7*os&e4)3h+2!xlh&!3vjt3yhtjies4@wbrl36t0xy!c8jf5"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
